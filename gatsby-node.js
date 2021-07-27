b30c7561e5bdb5c5eefa5d9b2c9d13a5bd6fb1a8
gauravsingh1012 <gauravsinghmail@gmail.com>
2021-07-27 15:34:47 +0530

    Initial commit from gatsby: (https://github.com/thegauravsingh/gatsby-pokedex.git)

---
+++ gatsby-node.js
@@ -0,0 +1,18 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const fetch = require('node-fetch');
+
+exports.sourceNodes = async() => {
+
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+    const json =  await response.json();
+    const {results =[]} = json;
+    
+    console.log('results',results);
+}
